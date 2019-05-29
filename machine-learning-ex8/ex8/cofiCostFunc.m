e271722c5a0c14e0e345de091838daee2ed39433
Wen, George <George.Wen@smec.com>
2019-05-29 13:06:08 +1000

    solution to ex8

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,17 +41,31 @@ Theta_grad = zeros(size(Theta));
 %
 
 
+J = sum(sum((((X * Theta' - Y).^2) .* R)))/2 + sum(sum(Theta.^2))*lambda/2 + sum(sum(X.^2))*lambda/2;
 
 
+%X_grad = (X * Theta' - Y) * Theta;
 
 
+for i =1:num_movies
 
+	idx = find(R(i,:)==1);
+	Theta_temp = Theta(idx,:);
+	Y_temp = Y(i,idx);	
+	X_grad(i,:) = (X(i,:) * Theta_temp' - Y_temp) * Theta_temp + lambda * X(i,:);
 
+endfor
 
+%Theta_grad = (X*Theta' -Y)' * X;
 
+for j=1:num_users
+	idx = find(R(:,j) ==1);
+    X_temp = X(idx,:);
+	Y_temp = Y(idx,j);
 
+	Theta_grad(j,:) = (X_temp * Theta(j,:)' - Y_temp)' * X_temp + lambda * Theta(j,:);
 
-
+endfor
 
 
 
--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -25,13 +25,16 @@ for epsilon = min(pval):stepsize:max(pval)
 
 
 
-
-
-
-
-
-
-
+	cvPredictions = pval < epsilon ; 
+	
+	tp = sum((cvPredictions ==1) & (yval==1));
+	fp = sum((cvPredictions ==1) & (yval==0));
+	fn = sum((cvPredictions ==0) & (yval==1));
+	
+	prec = tp /(tp + fp);
+	rec = tp/(tp + fn);
+	
+	F1 =  2 * prec * rec / (prec + rec);
 
 
 
